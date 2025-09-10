f1eda46c156d97e33b33cf4f74fd8543d72c814f
joaodepaulojp <joaodepaulocardoso@edu.unifor.br>
2025-09-09 21:14:15 -0300

    feat: add initial restaurant page and menu page

---
+++ src/app/[slug]/menu/page.tsx
@@ -0,0 +1,32 @@
+import { notFound } from "next/navigation";
+
+import { db } from "@/lib/prisma";
+
+import RestaurantHeader from "./components/header";
+
+interface RestaurantPageProps {
+  params: Promise<{ slug: string }>;
+  searchParams: Promise<{ consumptionMethod: string }>;
+}
+
+const isConsumptionMethodValid = (consumptionMethod: string) => {
+  return ["DINE_IN", "TAKEAWAY"].includes(consumptionMethod.toUpperCase());
+};
+
+const RestaurantPage = async ({
+  params,
+  searchParams,
+}: RestaurantPageProps) => {
+  const { slug } = await params;
+  const { consumptionMethod } = await searchParams;
+  if (!isConsumptionMethodValid(consumptionMethod)) {
+    return notFound();
+  }
+  const restaurant = await db.restaurant.findUnique({ where: { slug } });
+  if (!restaurant) {
+    return notFound();
+  }
+  return <RestaurantHeader restaurant={restaurant} />;
+};
+
+export default RestaurantPage;
